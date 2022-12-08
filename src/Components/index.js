db60b3b477530a773205b201de09372ef8498d9e
Bond, Austin Jackson <aubond@iu.edu>
2022-12-08 12:59:54 -0500

    Delete src directory

--- src/Components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as LandingScreen } from './LandingScreen';
-export { default as LoginScreen } from './LoginScreen';
-export { default as RegisterScreen } from './RegisterScreen';
-export { default as HomeScreen } from './HomeScreen';
-export { default as Navbar } from './Navbar';
-export { default as Sidebar } from './Sidebar';
-export { default as ExerciseScreen } from './ExerciseScreen'
